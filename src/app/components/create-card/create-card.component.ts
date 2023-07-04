084b2ba80f0d9feb4a68d3a3aff7e06e49f905e7
m1gue21 <miguefreeagent@gmail.com>
2023-07-04 16:07:53 -0500

    update validations in create form
    colors added, function createCard updated

--- src/app/components/create-card/create-card.component.ts
+++ src/app/components/create-card/create-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CreditCard } from 'src/app/models/CreditCard';
 
 @Component({
   selector: 'app-create-card',
@@ -12,17 +13,24 @@ export class CreateCardComponent implements OnInit {
   constructor(private fb: FormBuilder) {
     this.form = this.fb.group({
       owner: ['', Validators.required],
-      number: ['', Validators.required, Validators.minLength(16), Validators.maxLength(16)],
-      expDate: ['', Validators.required, Validators.minLength(5), Validators.maxLength(5)],
-      cvv: ['', Validators.required, Validators.minLength(3), Validators.maxLength(3)],
-    })
+      number: ['', [Validators.required, Validators.minLength(16), Validators.maxLength(16)]],
+      expDate: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(5)]],
+      cvv: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(3)]],
+    });
   }
 
   ngOnInit(): void {
   }
 
   createCard() {
-    console.log(this.form)
+    const CARD: CreditCard = {
+      owner: this.form.value.owner,
+      number: this.form.value.number,
+      expDate: this.form.value.expDate,
+      cvv: this.form.value.cvv,
+      creationDate: new Date(),
+      updatedAt: new Date()
+    }
+    console.log(CARD)
   }
-
 }
